9438c7705da43294a0f4a1ff6cd57f0e5cd66389
victorxu1121 <victorxu1121@126.com>
2017-05-15 02:27:48 -0700

    env_test producer&consumer

--- env_test/src/main.c
+++ env_test/src/main.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define PRODUCTION_MAX 10
 
 int cnt_temp_1;
 int cnt_temp_2;
@@ -12,11 +13,19 @@ int cnt_temp_3;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-//pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_quit = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_not_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_not_empty = PTHREAD_COND_INITIALIZER;
+
+int flag_full;
+int flag_empty;
+int production_cnt;
 
 void *thread1(void *);
 void *thread2(void *);
 void *thread3(void *);
+void *thread_producer(void *);
+void *thread_consumer(void *);
 
 void lock_thread(const char *s);
 void unlock_thread(const char *s);
@@ -57,7 +66,7 @@ int main(int argc, char * argv[])
 	//===cmd line init ==end=======//
 
 	//===pthread init ==start======//
-	pthread_t tid1,tid2,tid3;
+	pthread_t tid1,tid2,tid3,tid4,tid5;
 	int err_t1 = 0, err_t2 = 0, err_t3 = 0;
 	void **rval1;
 	void **rval2;
@@ -70,9 +79,11 @@ int main(int argc, char * argv[])
 	cnt_temp_2 = 0;
 	cnt_temp_3 = 0;
 
-//===pthread init ==end========//
-	
-	
+	//===pthread init ==end========//
+	flag_full = 0;
+	flag_empty = 1;
+	production_cnt = 0;
+	int producer_id, consumer_id;
 	
 	cl_cmd_menu_print();
 	while(1)
@@ -86,6 +97,7 @@ int main(int argc, char * argv[])
 		case 0: 
 			{
 				cl_printf("reset cmd got!");
+				production_cnt = 0;
 				break;
 			}
 		case 1: 
@@ -93,17 +105,39 @@ int main(int argc, char * argv[])
 				cl_printf("start cmd got!");
 				err_t1 = pthread_create(&tid1, NULL , thread1, NULL);
 				if (err_t1 != 0)
-					{printf("%s: %d\n", __func__, strerror(err_t1));}
+					{printf("Thread1 create failed!!\n");}///%s: %d\n", __func__, strerror(err_t1));}
 				printf("Thread 1 created.\n");
 				break;
 			}
 		case 2: 
 			{
 				cl_printf("run cmd got!");
+				/*
 				err_t2 = pthread_create(&tid2, NULL , thread2, NULL);
 				if (err_t2 != 0)
-					{printf("%s: %d\n", __func__, strerror(err_t2));}
+					{printf("Thread1 create failed!!\n");}///%s: %d\n", __func__, strerror(err_t2));}
 				printf("Thread 2 created.\n");
+				*/
+				producer_id = 1;
+				err_t2 = pthread_create(&tid2, NULL, thread_producer, (void*)&producer_id);
+				if (err_t2 != 0)
+					{printf("Thread producer create failed!!\n");}///%s: %d\n", __func__, strerror(err_t1));}
+				else 
+					{printf("Thread producer created.\n");}
+
+				consumer_id = 1;
+				err_t3 = pthread_create(&tid3, NULL, thread_consumer, (void*)&consumer_id);
+				if (err_t3 != 0)
+					{printf("Thread consumer create failed!!\n");}///%s: %d\n", __func__, strerror(err_t1));}
+				else 
+					{printf("Thread consumer created.\n");}
+
+				consumer_id = 2;
+				err_t3 = pthread_create(&tid4, NULL, thread_consumer, (void*)&consumer_id);
+				if (err_t3 != 0)
+					{printf("Thread consumer create failed!!\n");}///%s: %d\n", __func__, strerror(err_t1));}
+				else 
+					{printf("Thread consumer created.\n");}
 				break;
 			}
 		case 3: 
@@ -120,6 +154,7 @@ int main(int argc, char * argv[])
 		case 6: 
 			{
 				cl_printf("Quit!");
+				pthread_cond_signal(&cond_quit);
 				sleep(1);
 				return 0;
 			}
@@ -182,11 +217,13 @@ void *thread1(void *arg)
 					if (cnt_temp_3%10 == 0)
 					{
 						printf("Thread1: %d-%d-%d\n", cnt_temp_3, cnt_temp_2, cnt_temp_1);
+						pthread_cond_signal(&cond);
 					}
 				}
 			}
 		}
 		unlock_thread("Thread1");
+		pthread_cond_broadcast(&cond);
 	}
 	return;
 }
@@ -200,7 +237,8 @@ void *thread2(void *arg)
 		if(cl_cmd_id == 6)
 		{
 			printf("Thread2 end\n");
-			break;
+			//break;
+			return;
 		}
 		else if (cl_cmd_id == 0)
 		{
@@ -209,6 +247,7 @@ void *thread2(void *arg)
 		}
 		else 
 		{
+			pthread_cond_wait(&cond, &mutex);
 			if (cnt_temp_1%100 == 0)
 			{
 				if (cnt_temp_2%100 == 0)
@@ -243,4 +282,85 @@ void unlock_thread(const char *s)
         exit(0);
     }
 }
+
+void *thread_producer(void *arg)
+{
+	int *producer_id_thread = (int*) arg;
+	while(1)
+	{
+		if (cl_cmd_id == 6)
+		{
+			break;
+		}
+		
+		lock_thread("Thread producer");
+		if (flag_full == 1)
+		{
+			printf("Producer[%d]: waiting for consumer...\n", *producer_id_thread);
+			pthread_cond_wait(&cond_not_full,&mutex);
+		}
+		else 
+		{
+			production_cnt ++;
+			flag_empty = 0;
+			if (production_cnt == PRODUCTION_MAX)
+			{
+				flag_full = 1;
+				printf("Producer[%d]: Buffer is Full!(NUM = %d)\n",*producer_id_thread,production_cnt);
+			}
+			else 
+			{
+				flag_full = 0;
+				pthread_cond_signal(&cond_not_empty);
+				printf("Producer[%d]: Buffer is not Full!(NUM = %d flag_full = %d)\n",*producer_id_thread,production_cnt);
+
+			}
+		}
+		unlock_thread("Thread producer");
+		sleep(1);
+	}
+	return;
+}
+
+void *thread_consumer(void *arg)
+{
+	int *consumer_id_thread = (int*) arg;
+	while (1)
+	{
+		if (cl_cmd_id == 6)
+		{
+			break;
+		}
+
+		lock_thread("Thread consumer");
+		if (flag_empty == 1)
+		{
+			printf("Consumer[%d]: waiting for producer...\n", *consumer_id_thread);
+			pthread_cond_wait(&cond_not_empty, &mutex);
+		}
+		else
+		{
+			production_cnt --;
+			flag_full = 0;
+			if (production_cnt == 0)
+			{
+				flag_empty = 1;
+				printf("Consumer[%d]: Buffer is empty!(NUM = %d)\n",*consumer_id_thread,production_cnt);
+			}
+			else 
+			{
+				flag_empty = 0;
+				pthread_cond_signal(&cond_not_full);
+				printf("Consumer[%d]: Buffer is not empty!(NUM = %d)\n",*consumer_id_thread,production_cnt);
+			}
+		}
+		unlock_thread("Thread consumer");
+		sleep(3);
+	}
+	return;
+}
+
+
+
+
 
